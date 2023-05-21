dc3ac03f36f35ab775915c3cddbbd018b79722f1
Sebas024 <charles.deo024@hotmail.ca>
2023-05-21 07:57:10 -0600

    me

--- sum.js
+++ sum.js
@@ -1,2 +1,7 @@
-const args = process.argv;
-console.log(args);
+let sum = 0;
+let x = 0;
+for (let j = 2; j < process.argv.length; j++) {
+  x = Number(process.argv[j]);
+  sum = sum + x;
+}
+console.log(sum);
