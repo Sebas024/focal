475a15d064759b3d69fe169e2b34b36999d0c557
Charles Samonte <charles.deo024@hotmail.ca>
2023-05-22 23:24:48 -0600

    reading error messages and fixing bugs

--- average.js
+++ average.js
@@ -1,14 +1,26 @@
-function average(list) {
-  var sum = 0;
+// function average(list) {
+//   var sum = 0;
 
-  for (var num of list) {
-    sum += num;
+//   for (var num of list) {
+//     sum += num;
+//   }
+
+//   return sum / list.length;
+// }
+// console.log(average([3, 5, 7]));
+
+
+
+
+let average = function(list) {
+  let sum = 0;
+
+  for (let num of list) {
+    // sum += num;
+    sum = sum + num;
   }
 
   return sum / list.length;
-}
+};
+
 console.log(average([3, 5, 7]));
-
-
-
-
