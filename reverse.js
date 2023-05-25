44eb27178743c6c1ace209c40934b8e3aee450f9
Charles Samonte <charles.deo024@hotmail.ca>
2023-05-24 21:23:26 -0600

    Debugging Errors

--- reverse.js
+++ reverse.js
@@ -1,9 +1,9 @@
-var input = process.argv[2];
+let input = process.argv[2];
 
 if (input) {
   console.log(reverse(input));
-};
+}
 
-function reverse(original) {
+const reverse = function(original) {
   return original.split('').reverse().join('');
 };
